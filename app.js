4d5e52f745699b97a6de7c7a56d83f044460e036
david vu <davidvu@davids-MacBook-Pro.local>
2018-06-05 09:01:23 -0600

    add all files

---
+++ app.js
@@ -0,0 +1,15 @@
+var  express = require('express'),
+	 app = express();
+
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+  res.render("home");
+})
+ 
+app.listen(3000, function (){
+	console.log("Server started on port 3000");
+})
