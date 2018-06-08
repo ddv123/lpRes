e0eeeac138955dfbd32a8c9d9145553cb1e63d93
david vu <davidvu@davids-MacBook-Pro.local>
2018-06-08 13:16:41 -0600

    change port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var  express = require('express'),
 	 app = express();
 
-
-app.use(express.static('public'));
+var port = process.env.PORT || 8080;
+app.use(express.static(__dirname +'/public'));
 
 app.set("view engine", "ejs");
 
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
   res.render("home");
 })
  
-app.listen(3000, function (){
-	console.log("Server started on port 3000");
+app.listen(port, function (){
+	console.log("Server started");
 })
